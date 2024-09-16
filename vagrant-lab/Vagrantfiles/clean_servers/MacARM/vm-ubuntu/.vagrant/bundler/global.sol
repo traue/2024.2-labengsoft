40f4e24ea7b3b4000af1a9e059695e9c57cc94a1
Thiago G. Traue <t.traue@gmail.com>
2024-09-16 10:58:02 -0300

    vagrant lab

---
+++ vagrant-lab/Vagrantfiles/clean_servers/MacARM/vm-ubuntu/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["vagrant-qemu",["= 0.3.5"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]],["vagrant-vmware-desktop",["= 3.0.1"]]],"checksum":"6ac7dd9b389fca44f20b86c70569df105ce3bf4f2e7f5b8e042f11450af9339f","vagrant_version":"2.4.1"}
